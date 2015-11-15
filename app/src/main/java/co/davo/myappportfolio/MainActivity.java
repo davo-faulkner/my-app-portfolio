2dd92fc21538d17d402acd5db8e65b9a9c58c64d
Davo Faulkner <d@davo.co>
2015-11-15 09:18:00 -0600

    First add

---
+++ app/src/main/java/co/davo/myappportfolio/MainActivity.java
@@ -0,0 +1,80 @@
+package co.davo.myappportfolio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    public void displaySpotifyToast(View v) {
+        Toast toast = Toast.makeText(this, "This button will launch my spotify app!",
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void displayScoresToast(View v) {
+        Toast toast = Toast.makeText(this, "This button will launch my scores app!",
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void displayLibraryToast(View v) {
+        Toast toast = Toast.makeText(this, "This button will launch my library app!",
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void displayBuildItToast(View v) {
+        Toast toast = Toast.makeText(this, "This button will launch my build it app!",
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void displayXyzToast(View v) {
+        Toast toast = Toast.makeText(this, "This button will launch my xyz reader app!",
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void displayCapstoneToast(View v) {
+        Toast toast = Toast.makeText(this, "This button will launch my capstone app!",
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
